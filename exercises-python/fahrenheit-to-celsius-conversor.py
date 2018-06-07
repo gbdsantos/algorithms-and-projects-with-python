5c0105584f750933b65bf24bd6e0bfa6f8fd9e69
Guilherme Bezerra <gbsantos.it@gmail.com>
2018-06-07 13:32:48 -0300

    Initial Commit

---
+++ exercises-python/fahrenheit-to-celsius-conversor.py
@@ -0,0 +1,4 @@
+temperatureFahrenheit = 78
+temperatureCelsius = (temperatureFahrenheit - 32) * 5 / 9
+
+print("A temperatura em celsius é", temperatureCelsius)
---
+++ exercises-python/slicing.py
@@ -0,0 +1,9 @@
+myString = 'abcdefghijk'
+
+myString[:1]
+
+myString[1:]
+
+myString[2:7:2]
+
+myString[::3]
