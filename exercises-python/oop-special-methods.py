76512f602627371630e129df152823d760e6e5ca
Guilherme Bezerra <gbsantos.it@gmail.com>
2018-06-29 15:31:07 -0300

    OOP - Special(Magic/Dunder) Methods

---
+++ exercises-python/oop-special-methods.py
@@ -0,0 +1,34 @@
+mylist = [1, 2, 3]
+
+len(mylist)
+
+class Sample():
+    pass
+
+mysample = Sample()
+
+len(mysample)
+
+
+class Book():
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("A book object has been deleted")
+
+b = Book('Python rocks', 'Jose' ,200)
+
+print(b)
+len(b)
+
+# Deleted Object
+del b
