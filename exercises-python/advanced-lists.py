ff8afc8dae7c14d235bf89c05c8117472ece6331
Guilherme Bezerra <gbsantos.it@gmail.com>
2018-07-25 14:56:38 -0300

    Advanced Lists

---
+++ exercises-python/advanced-lists.py
@@ -0,0 +1,27 @@
+l = [1, 2, 3]
+l.append(4)
+l
+
+l.count(10)
+l.count(1)
+
+x = [1, 2, 3]
+x.append([4,5])
+print x
+
+x [1, 2, 3]
+x.extend([4,5])
+print x
+
+l.index(2)
+l.insert(2, 'inserted')
+
+
+ele = l.pop()
+ele
+
+l
+
+l.pop(0)
+l.remove('inserted')
+
