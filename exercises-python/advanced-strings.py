d50f76e411c02c28edf2d9f5eeb7f166115372ac
Guilherme Bezerra <gbsantos.it@gmail.com>
2018-07-25 14:34:46 -0300

    Advanced Strings

---
+++ exercises-python/advanced-strings.py
@@ -0,0 +1,28 @@
+s = 'hello world'
+
+s.capitalize()
+s.upper()
+s.lower()
+s.count('o')
+s.find('o')
+
+s
+s.center('20','z')
+
+print 'hello\thi'
+
+'hello\thi'.expandtabs()
+
+s = 'hello'
+s.isalnum()
+s.isalpha()
+s.islower()
+s.isspace()
+s.istitle()
+s.isupper()
+s.endswith('o')
+s[-1] == 'o'
+
+s.split('e')
+s.partition()
+
